01836ca27231dea97d7388d35cb2c13c7da863a7
Leandro1Ezequiel1 <81895143+Leandro1Ezequiel1@users.noreply.github.com>
2023-03-01 18:42:27 -0300

    a

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-
-const { ProductManager } = require('./productManager')
+const ProductManager = require("./products/products")
 const productManager = new ProductManager();
+productManager.addProduct('Lona Zonda 900', 'Descripción del producto 1', 10, '.jpg', '001', 100);
+productManager.addProduct('Lona Trasnparente', 'Descripción del producto 2', 20, 'jpg', '002', 50);
+productManager.addProduct('Coversol', 'Descripción del producto 3', 30, 'jpg', '003', 200);
 
-productManager.addProduct('Producto 1', 'Descripción del producto 1', 10, 'ruta-de-imagen-1.jpg', '001', 100);
+const productList = productManager.getProducts();
+
+for (const product of productList) {
+  console.log(`ID: ${product.id}`);
+  console.log(`Título: ${product.title}`);
+  console.log(`Descripción: ${product.description}`);
+  console.log(`Precio: ${product.price}`);
+  console.log(`Thumbnail: ${product.thumbnail}`);
+  console.log(`Código: ${product.code}`);
+  console.log(`Stock: ${product.stock}`);
+  console.log('---------------------');
+}
 
 
+
+    
+  
